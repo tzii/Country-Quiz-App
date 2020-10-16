10a1da4df577c7a4b3ba67561dbb3f55061eb526
Tuấn Vũ <tuanvundvn@gmail.com>
2020-10-16 23:11:43 +0700

    Updates

---
+++ precache-manifest.862c8637a7f5ef6c62ff052a6bd34068.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8162e8781654dd4ca11f8e901f493508",
+    "url": "/Country-Quiz-App/index.html"
+  },
+  {
+    "revision": "1a89f5fbfe076185f69e",
+    "url": "/Country-Quiz-App/static/css/main.32dbc198.chunk.css"
+  },
+  {
+    "revision": "9e86ccaef9debf983d8a",
+    "url": "/Country-Quiz-App/static/js/2.1dcbb194.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Country-Quiz-App/static/js/2.1dcbb194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a89f5fbfe076185f69e",
+    "url": "/Country-Quiz-App/static/js/main.bcc45344.chunk.js"
+  },
+  {
+    "revision": "83bd13860b7c1d82fae0",
+    "url": "/Country-Quiz-App/static/js/runtime-main.1de1e4f9.js"
+  },
+  {
+    "revision": "eae25da4c02c031beef455d31ee43749",
+    "url": "/Country-Quiz-App/static/media/1.eae25da4.svg"
+  },
+  {
+    "revision": "82d9605afcc8a7a321691203895e1aa3",
+    "url": "/Country-Quiz-App/static/media/win.82d9605a.svg"
+  }
+]);
